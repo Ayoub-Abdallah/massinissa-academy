5ee542c7ebf583d2443bcff4cce0fb69524109a8
Ayoub-Abdallah <138675957+Ayoub-Abdallah@users.noreply.github.com>
2025-02-15 09:55:09 +0100

    Update admin.js

--- models/admin.js
+++ models/admin.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const adminSchema = new Schema({
     firstname: { type: String, required: true },
     lastname: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: false, unique: true },
     phone: { type: Number, required: true },
     password: { type: String, required: true },
     roles: { type: String, required: true, enum: ['superadmin', 'admin'] }
